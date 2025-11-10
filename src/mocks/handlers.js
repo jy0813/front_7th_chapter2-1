fc3fa882e0b2b5eaa528d2e2414fbce9d86bacf5
jy0813 <busbug3@gmail.com>
2025-11-10 20:03:31 +0900

    chore: prettier 정리

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -27,7 +27,9 @@ function filterProducts(products, query) {
   if (query.search) {
     const searchTerm = query.search.toLowerCase();
     filtered = filtered.filter(
-      (item) => item.title.toLowerCase().includes(searchTerm) || item.brand.toLowerCase().includes(searchTerm),
+      (item) =>
+        item.title.toLowerCase().includes(searchTerm) ||
+        item.brand.toLowerCase().includes(searchTerm),
     );
   }
 
